175dc389cbb95a28f5fcf3cfb74d6af0bb02de83
nabepa <nabepadev@gmail.com>
2021-07-20 10:32:23 +0900

    start client side

---
+++ client/src/components/views/NavBar/NavBar.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NavBar = (props) => <h1>NavBar</h1>;
+
+export default NavBar;
---
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const RegisterPage = (props) => <h1>Register Page</h1>;
+
+export default RegisterPage;
